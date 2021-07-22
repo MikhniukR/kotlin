606c6ec778d8d4da0f365856c9b23e4ebb7d218c
Andrey Zinovyev <andrey.zinovyev@jetbrains.com>
2021-07-22 14:55:52 +0300

    [FIR] Add ILLEGAL_INLINE_PARAMETER_MODIFIER diagnostic

--- idea/idea-frontend-fir/src/org/jetbrains/kotlin/idea/frontend/api/fir/diagnostics/KtFirDiagnostics.kt
+++ idea/idea-frontend-fir/src/org/jetbrains/kotlin/idea/frontend/api/fir/diagnostics/KtFirDiagnostics.kt
@@ -2230,6 +2230,10 @@ sealed class KtFirDiagnostic<PSI : PsiElement> : KtDiagnosticWithPsi<PSI> {
         override val diagnosticClass get() = InlinePropertyWithBackingField::class
     }
 
+    abstract class IllegalInlineParameterModifier : KtFirDiagnostic<KtElement>() {
+        override val diagnosticClass get() = IllegalInlineParameterModifier::class
+    }
+
     abstract class CannotAllUnderImportFromSingleton : KtFirDiagnostic<KtImportDirective>() {
         override val diagnosticClass get() = CannotAllUnderImportFromSingleton::class
         abstract val objectName: Name
